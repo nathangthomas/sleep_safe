166bca5b857707f17314ad56b82d4399f2f5c901
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-09-02 16:22:20 -0600

    Add feature tests for nav bar and shelter index

--- spec/features/user_can_see_nav_bar_spec.rb
+++ spec/features/user_can_see_nav_bar_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe 'user can see nav bar' do
   describe 'as a user when I visit the welcome page I see a nav bar on the top of the page' do
-    describe 'it has a field to search for shelters by zipcode and '
+    describe 'it has a field to search for shelters by zipcode and an optional radius' do
+      it 'there is also a search button' do
 
-
-In the nav bar on the left side I see a search feature with two fields (zip code and radius) that searches for shelters by zip code and allows for an optional radius.
-
-I also see a search button
+        visit '/'
+        within '.nav-bar'do
+          expect(find_field('zip_code').value).to eq 'zip code'
+          expect(find_field('radius').value).to eq 'radius'
+          expect(page).to have_button("Search")
+        end
+      end
+    end
+  end
+end
