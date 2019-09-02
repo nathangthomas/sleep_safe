6365f05540099c3003eea8927a5a9d802229cf57
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-09-02 15:33:26 -0600

    Add gem spring

---
+++ spec/features/user_can_see_list_of_shelters_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'User can see list of shelters' do
+  describe 'when I search for shelters by zipcode then I am taken to shelters/index where I see all shelters' do
+    it 'I see the name, address, and the hours of opperation' do
+
+    
+    end
+  end
+end
