4b4ddab758e56bd2be0669d8da0ffa1f76d83a5f
username <wnt925@gmail.com>
2019-09-02 22:47:31 -0600

    Add login/register/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
+  root "home#index"
+  get '/dashboard', to: 'users#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
 end
