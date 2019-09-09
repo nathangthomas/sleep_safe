48d1b92841fa37fcd42b7843e90fcad4f3282a9f
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-09-09 12:31:36 -0600

    Update css on shelter index page

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -65,6 +65,7 @@ class SheltersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_shelter
+      binding.pry
       @shelter = Shelter.find(params[:id])
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :shelters
   root "home#index"
+
   get '/search', to: "search#index"
+
   get '/dashboard', to: 'users#show', as: 'dashboard'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
