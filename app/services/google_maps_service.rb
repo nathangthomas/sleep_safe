f88932b485a71589c340a25d0ba134b855af1d72
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-10 14:59:31 -0600

    refactors conn method from api calls

--- app/services/google_maps_service.rb
+++ app/services/google_maps_service.rb
@@ -6,45 +6,39 @@ class GoogleMapsService
   end
 
   def coordinates
-    conn = Faraday.new(url: "https://maps.googleapis.com") do |faraday|
-      faraday.params["key"] = ENV["GOOGLE_PLACES_API_KEY"]
-      faraday.params["components"] = "postal_code:#{@zip_code}"
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("https://maps.googleapis.com/maps/api/geocode/json?")
+    conn_params = {"components":"postal_code:#{@zip_code}"}
+    response = conn(conn_params).get("https://maps.googleapis.com/maps/api/geocode/json?")
     data = JSON.parse(response.body, symbolize_names: true)
     data[:results][0][:geometry][:location].values.join(",")
   end
 
-
   def get_place_ids
     location = coordinates
-    conn = Faraday.new(url: "https://maps.googleapis.com") do |faraday|
-      faraday.params["key"] = ENV["GOOGLE_PLACES_API_KEY"]
-      faraday.params["radius"] = @radius
-      faraday.params["keyword"] = "homeless shelter"
-      faraday.params["location"] = location
-      faraday.adapter Faraday.default_adapter
-    end
-
-    basic_response = conn.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
-    basic_shelter_data = JSON.parse(basic_response.body, symbolize_names: true)
-    basic_shelter_data[:results].map{|shelter| shelter[:place_id]}
+    conn_params = {"radius": @zip_code, "keyword": "homeless shelter", "location": location}
+    response = conn(conn_params).get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
+    place_ids = JSON.parse(response.body, symbolize_names: true)
+    place_ids[:results].map{|shelter| shelter[:place_id]}
   end
 
   def shelters_by_zip_code
     shelter_place_ids = get_place_ids
-    shelter_place_ids.map do |i|
-      conn = Faraday.new(url: "https://maps.googleapis.com") do |faraday|
-        faraday.params["key"] = ENV["GOOGLE_PLACES_API_KEY"]
-        faraday.params["placeid"] = i
-        faraday.params["fields"] = "name,formatted_address,formatted_phone_number,opening_hours,geometry"
-        faraday.adapter Faraday.default_adapter
-      end
-      detailed_response = conn.get("https://maps.googleapis.com/maps/api/place/details/json?")
-      detailed_shelter_data = JSON.parse(detailed_response.body, symbolize_names: true)
+    shelter_place_ids.map do |id|
+      conn_params = {"placeid": id, "fields": "name,formatted_address,formatted_phone_number,opening_hours,geometry"}
+      response = conn(conn_params).get("https://maps.googleapis.com/maps/api/place/details/json?")
+      detailed_shelter_data = JSON.parse(response.body, symbolize_names: true)
       Shelter.new(detailed_shelter_data[:result])
+    end
+  end
+
+  private
+
+  def conn(conn_params)
+    Faraday.new(url: "https://maps.googleapis.com") do |faraday|
+      faraday.params["key"] = ENV["GOOGLE_PLACES_API_KEY"]
+      conn_params.each do |key, value|
+        faraday.params[key] = "#{value}"
+      end
+      faraday.adapter Faraday.default_adapter
     end
   end
 end
