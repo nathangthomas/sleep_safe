d8bad44335e7c339113b040194e6a81fd38a47d9
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-11 16:51:08 -0600

    creates shelter_creator class and modifies shelter_create rake file to use it accordingly

--- app/models/shelter_creator.rb
+++ app/models/shelter_creator.rb
@@ -1,25 +1,14 @@
-class SheltersCreator
-  attr_reader :place_id, :name, :address, :zip, :phone_number, :hours, :lattitude, :longitude
+class ShelterCreator
 
-  def initialize(attr={})
-    @place_id = place_id
-    @name = detailed_shelter_data[:result][:name]
-    @address = detailed_shelter_data[:result][:formatted_address]
-    @zip = detailed_shelter_data[:result][:formatted_address].split(',')[2].split(' ')[1]
-    @phone_number = detailed_shelter_data[:result][:formatted_phone_number]
-    @hours = detailed_shelter_data[:result][:opening_hours]
-    @latitude = detailed_shelter_data[:result][:geometry][:location][:lat]
-    @longitude = detailed_shelter_data[:result][:geometry][:location][:lng]
-  end
-
-  def create_shelter(detailed_shelter_data)
-    name = detailed_shelter_data[:result][:name]
-    address = detailed_shelter_data[:result][:formatted_address]
-    zip = detailed_shelter_data[:result][:formatted_address].split(',')[2].split(' ')[1]
-    phone_number = detailed_shelter_data[:result][:formatted_phone_number]
-    hours = detailed_shelter_data[:result][:opening_hours]
-    lat = detailed_shelter_data[:result][:geometry][:location][:lat]
-    lng = detailed_shelter_data[:result][:geometry][:location][:lng]
-    Shelter.create!(name: name, address: address, phone_number: phone_number, hours: hours, latitude: lat, longitude: lng, zip: zip)
+  def create_record(shelter_data)
+    name = shelter_data[:name]
+    address = shelter_data[:formatted_address]
+    zip = shelter_data[:formatted_address].split(',')[2].split(' ')[1]
+    phone_number = shelter_data[:formatted_phone_number]
+    hours = shelter_data[:opening_hours]
+    lat = shelter_data[:geometry][:location][:lat]
+    lng = shelter_data[:geometry][:location][:lng]
+    place_id =
+    Shelter.create!(name: name, address: address, phone_number: phone_number, hours: hours, latitude: lat, longitude: lng, zip: zip, place_id: place_id)
   end
 end
--- lib/tasks/shelter_create.rake
+++ lib/tasks/shelter_create.rake
@@ -11,7 +11,10 @@ namespace :import do
 
     denver_zipcodes.each do |zip|
       service = GoogleMapsService.new(zip, 10)
-      service.shelters_by_zip_code
+      service.shelters_by_zip_code.map do |shelter_data|
+        creator = ShelterCreator.new
+        creator.create_record(shelter_data)
+      end
     end
   end
 end
