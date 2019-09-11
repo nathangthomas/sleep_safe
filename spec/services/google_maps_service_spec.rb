5a6320a74ff17a7cd0a4645a3c336b0e4486c2ce
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-11 10:01:55 -0600

    adapts test to working with database

--- spec/services/google_maps_service_spec.rb
+++ spec/services/google_maps_service_spec.rb
@@ -5,18 +5,19 @@ describe GoogleMapsService do
   context "instance methods" do
     context "shelters by zip_code" do
       it "returns homeless shelter data" do
+        WebMock.allow_net_connect!
         service = GoogleMapsService.new("80202", 3218.68)
         shelters = service.shelters_by_zip_code
         expect(shelters).to be_a Array
         expect(shelters.count).to eq(20)
         shelter_data = shelters[0]
 
-        expect(shelter_data.name).to be
-        expect(shelter_data.address).to be
-        expect(shelter_data.phone_number).to be
-        expect(shelter_data.hours).to be
-        expect(shelter_data.lat).to be
-        expect(shelter_data.lng).to be
+        expect(shelter_data[:name]).to be
+        expect(shelter_data[:address]).to be
+        expect(shelter_data[:phone_number]).to be
+        expect(shelter_data[:hours]).to be
+        expect(shelter_data[:latitude]).to be
+        expect(shelter_data[:longitude]).to be
       end
     end
   end
