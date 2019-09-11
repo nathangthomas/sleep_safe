7c7f571f106e901a68b3be30e8790b2ddc8ee9f5
Tay <tay.james.d@gmail.com>
2019-09-11 16:33:25 -0600

    adds helper method c_user (current user--had to override devise)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  helper_method :c_user
+
+  def c_user
+    @c_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country_code, :phone_number, :opt_in])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :country_code, :phone_number, :opt_in])
   end
-  
 end
