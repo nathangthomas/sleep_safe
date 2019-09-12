85f70136a4d73694598a91ee43dc3aee80dde500
Tay <tay.james.d@gmail.com>
2019-09-12 14:25:02 -0600

    Adds current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
+  def current_user
+   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country_code, :phone_number, :opt_in])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :country_code, :phone_number, :opt_in])
   end
-  
+
 end
