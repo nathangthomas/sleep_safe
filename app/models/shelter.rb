47d47512df6ebd4d93467f5477595e957928fda4
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-12 21:13:37 -0600

    small patch to validations to enable rake
    Co-authored-by: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>
    Co-authored-by: Tay DeHerrera <47490116+tayjames@users.noreply.github.com>

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,7 +1,7 @@
 class Shelter < ApplicationRecord
 
 validates :place_id, uniqueness: true
-validates_presence_of :name, :address, :phone_number, :hours, :latitude, :longitude, :zip
+validates_presence_of :name, :address, :phone_number, :latitude, :longitude
 
 geocoded_by :zip
 after_validation :geocode, :if => :address_changed?
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -5,10 +5,8 @@ RSpec.describe Shelter, type: :model do
    it {should validate_presence_of :name}
    it {should validate_presence_of :address}
    it {should validate_presence_of :phone_number}
-   it {should validate_presence_of :hours}
    it {should validate_presence_of :latitude}
    it {should validate_presence_of :longitude}
-   it {should validate_presence_of :zip}
    it {should validate_uniqueness_of :place_id}
   end
 end
