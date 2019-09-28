12d3863ee171e2b7c4398c9e0b459afd12f88d4b
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-28 16:32:39 -0600

    refactors shelter_create task and shelter_creator poro

--- app/models/shelter_creator.rb
+++ app/models/shelter_creator.rb
@@ -11,4 +11,14 @@ class ShelterCreator
     place_id = shelter_data[:place_id]
     Shelter.create!(name: name, address: address, phone_number: phone_number, hours: hours, latitude: lat, longitude: lng, zip: zip, place_id: place_id)
   end
+
+  def populate_shelters(zip_codes)
+    zip_codes.each do |zip|
+      service = GoogleMapsService.new(zip, 16093.4)
+      service.shelters_by_zip_code.map do |shelter_data|
+        create_record(shelter_data)
+      end
+    end
+  end
+
 end
